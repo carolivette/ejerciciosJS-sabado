a541e2b6564eee8cb1e4edfbfdd42ab96b997faf
CarolIvette <rcarolivette@gmail.com>
2023-11-04 13:20:01 -0600

    primer commit

---
+++ ejercicio.js
@@ -0,0 +1,8 @@
+
+letinvitados=[];
+let cantidadinvitados=0;
+do{
+   let invitados=prompt("Como te llamas");
+   invitados[cantidadinvitados]=invitado;
+   cantidadinvitados=cantidadinvitado
+}while
